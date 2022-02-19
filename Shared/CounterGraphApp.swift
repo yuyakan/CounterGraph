cedcb00d5d56b5f3bf70a2dbab728e19d7276496
yuyakan <kanbe1365@icloud.com>
2022-02-19 17:40:43 +0900

    Initial Commit

---
+++ Shared/CounterGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterGraphApp.swift
+//  Shared
+//
+//  Created by 上別縄祐也 on 2022/02/19.
+//
+
+import SwiftUI
+
+@main
+struct CounterGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
